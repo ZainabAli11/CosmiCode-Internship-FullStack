e8d2223e16a3270a16e55f505403ab7e049f4a54
ZainabAli11 <zainabali.101032@gmail.com>
2025-07-20 19:51:03 +0500

    WEEK 5 FULL STACK
    WEEK 5 FULL STACK

---
+++ WEEK 5 FULL STACK/TASK 2 JWT/task2/routes/private.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const verifyToken = require('../middleware/authMiddleware')
+const router = express.Router()
+
+router.get('/profile', verifyToken, (req, res) => {
+  res.json({ message: 'Welcome to your profile', userId: req.user.userId })
+})
+
+module.exports = router

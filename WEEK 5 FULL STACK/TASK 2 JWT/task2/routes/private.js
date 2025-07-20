6a86e11e8a931ee117e41f408fec6ba6e0689c79
ZainabAli11 <zainabali.101032@gmail.com>
2025-07-20 19:57:38 +0500

    Delete WEEK 5 FULL STACK directory

--- WEEK 5 FULL STACK/TASK 2 JWT/task2/routes/private.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const verifyToken = require('../middleware/authMiddleware')
-const router = express.Router()
-
-router.get('/profile', verifyToken, (req, res) => {
-  res.json({ message: 'Welcome to your profile', userId: req.user.userId })
-})
-
-module.exports = router
